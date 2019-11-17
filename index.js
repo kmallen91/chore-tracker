b439db373009f35ddb8c55d15c1794caee5948c5
Kevin Allen <kevin.allen91@gmail.com>
2019-11-17 15:47:22 -0800

    added tokens to login/register, user model, endpoints

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server");
 
-const port = env.process.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
