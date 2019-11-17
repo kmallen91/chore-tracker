5ce611a199322eac0e249a56cb64acfd562438d8
Kevin Allen <kevin.allen91@gmail.com>
2019-11-17 12:47:28 -0800

    initialized backend

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authRouter = require("../auth/authRouter.js");
+const userRouter = require("../users/userRouter.js");
+const choresRouter = require("../chores/choresRouter.js");
+const childrenRouter = require("../children/childrenRouter.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+module.exports = server;
