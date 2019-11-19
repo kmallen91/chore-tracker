40422ad71796c8252825261ba03994418f0289f3
Kevin Allen <kevin.allen91@gmail.com>
2019-11-19 14:50:29 -0800

    added validation middlewhere to /children route, edit delete message on child delete

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const validateUser = require("../middleware/validateUser");
+
 const userRouter = require("../users/userRouter");
 const choresRouter = require("../chores/choresRouter.js");
 const childrenRouter = require("../children/childrenRouter.js");
@@ -14,7 +16,7 @@ server.use(express.json());
 
 server.use("/users", userRouter);
 server.use("/chores", choresRouter);
-server.use("/children", childrenRouter);
+server.use("/children", validateUser, childrenRouter);
 
 server.get("/", (req, res) => {
   res.send("Server running");
